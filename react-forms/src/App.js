507e29bfb0b53bb7702714077f229ada820978cf
Michael Gastello <michaelgastello@aol.com>
2023-03-20 14:33:35 -0400

    working on form

--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import Form from "./form";
+import { Route, Switch } from 'react-router';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    
+    <div>
+      <h1>Hello From App</h1>
+      <Link to='/form'>Form</Link>
+
+      <Switch>
+        <Route path='/form' component={Form} />
+      </Switch>
+    </div>
   );
 }
 
---
+++ react-forms/src/form.jsx
@@ -0,0 +1,30 @@
+// import { useState, useRef } from 'react';
+
+const Form = () => {
+    return (
+        <>
+            <Form>
+                <input type="text" />
+                <input type="email" />
+                <input type="tel" />
+                {/* <label htmlFor="phone-type">Select Phone Type</label>
+                <select id="phone-type">
+                    <option value="home">Home</option>
+                    <option value="work">Work</option>
+                    <option value="mobile">Mobile</option>
+                </select>
+                <label htmlFor="instructor">Instructor</label>
+                <input type="radio" id='instructor' value='instructor' />
+                <label htmlFor="student">Student</label>
+                <input type="radio" id='student' value='student' />
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <input type="checkbox" id='notification' name='notification' />
+                <label htmlFor="notification">Sign up for e-mail notifications!</label>
+                <br />
+                <input type="submit" value='Submit' /> */}
+            </Form>
+        </>
+    )
+}
+
+export default Form;
